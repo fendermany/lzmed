2d5958b3a6f65ae22e885b4b06847744832c94a8
Dmitry <fendermany@gmail.com>
2021-07-11 15:34:05 +0300

    finish

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,1 @@
-// @@include('files/regular.js', {})
 @@include('files/functions.js', {})
-// @@include('files/script.js', {})
-// @@include('files/forms.js', {})
-// @@include('files/scroll.js', {})
